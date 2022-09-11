1309456d9e786c7f593355485636ccc5d2e66cd5
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2022-09-11 10:35:24 -0400

    intro book chapter 2: variables

---
+++ 2.Variables/name.rb
@@ -0,0 +1,10 @@
+puts "type your first name"
+first_name = gets.chomp
+
+puts "type your last name"
+last_name = gets.chomp
+
+puts "Hello, " + first_name + " " + last_name
+
+
+#10.times { puts name }
